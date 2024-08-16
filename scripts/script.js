118f0ff89b7b897df9421e7d07426b99b583036b
valdirjunior <valdirrugiskijr@gmail.com>
2024-08-16 11:01:28 -0300

    comm

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-console.log("Funfionou");
+const mensagem = "Funfionou";
+const posts = () => {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .catch(err => console.log(err));
+}
+        
+
+export {mensagem, posts};
